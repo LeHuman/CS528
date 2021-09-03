ed7a954b854b93844a46b34f80b6d69113eb6cd1
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-09-03 18:49:21 -0500

    move func

--- HW1/Interpret.py
+++
@@ -1,8 +0,0 @@
-from User import User
-
-def interpretData(data : str):
-    userList = list()
-    for line in data.splitlines():
-        args = line.split(',')
-        userList.append(User(*args))
-    print(userList)
--- HW1/main.py
+++ HW1/main.py
@@ -1,14 +1,23 @@
 from User import User
-from Interpret import interpretData
+
+
+def interpretData(data: str):
+    userList = list()
+    for line in data.splitlines():
+        args = line.split(",")
+        userList.append(User(*args))
+    print(userList)
+
 
 def main():
     with open("Data/adult.data") as f:
         userList = list()
         for line in f.readlines():
-            args = line.strip('\n').split(',')
+            args = line.strip("\n").split(",")
             if len(args) == 15:
                 userList.append(User(*args))
         for user in userList:
             print(user)
+
 
 main()
