7701d87ad1829f1287c638bddb28a6f8bef884a7
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-09-03 19:20:24 -0500

    check for unknown occupation

--- HW1/User.py
+++ HW1/User.py
@@ -29,15 +29,20 @@ class User:
         native_country,
         salary,
     ):
+
         self.age = age.strip()
         self.education = education.strip()
         self.marital_status = marital_status.strip()
-        self.occupation = occupation.strip()
+        self.occupation = None if occupation.strip() is "?" else occupation.strip()
         self.race = race.strip()
-        self.k_min = 10 if "<=50K" in salary else 5
+        self.k_min = 10 if "<=50K" in salary.strip() else 5
+
         global UID
         self.UID = UID
         UID += 1
 
     def __str__(self) -> str:
         return f"ID:{self.UID}\tK:{self.k_min}\n\tage: {self.age}\n\teducation: {self.education}\n\tmarital_status: {self.marital_status}\n\toccupation: {self.occupation}\n\trace: {self.race}"
+
+    def attributes(self):
+        return (self.age, self.education, self.marital_status, self.occupation, self.race)
