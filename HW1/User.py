15001a51e990accc03a4d8ecf48834739eff31ce
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-09-11 22:57:07 -0500

    introduce l-diversity

--- HW1/User.py
+++ HW1/User.py
@@ -1,3 +1,4 @@
+from typing import Hashable
 from Attr import Age, Attribute, Education, MaritalStatus, Race, Occupation
 
 UID = 0
@@ -6,14 +7,21 @@ UID = 0
 class User:
     UID = None
     k_min = 0
+    l_min = None
+    c_min = None
     count = 1
 
     age: Age
     education: Education
     marital_status: MaritalStatus
+    _occupation: str
     race: Race
 
+    # r_1, ..., r_m
     occupation: set
+
+    # every User grouped under this user
+    userSet: set
 
     def __init__(
         self,
@@ -32,6 +40,9 @@ class User:
         hours_per_week,
         native_country,
         salary,
+        k=None,
+        l=None,
+        c=None,
     ):
 
         self.age = Age(age.strip())
@@ -39,14 +50,25 @@ class User:
         self.marital_status = MaritalStatus(marital_status.strip())
         self.race = Race(race.strip())
 
-        self.occupation = set() if occupation.strip() == "?" else set([Occupation(occupation.strip())])
-        self.k_min = 10 if "<=50K" in salary.strip() else 5
+        self._occupation = occupation.strip()
+        self.occupation = set() if self._occupation == "?" else set([Occupation(self._occupation)])
+
+        self.userSet = set([self])
+
+        if k:
+            self.k_min = k
+        else:
+            self.k_min = 10 if "<=50K" in salary.strip() else 5  # set user preference
+
+        # No user preference for these values
+        self.l_min = l if l else None
+        self.c_min = c if c else None
 
         global UID
         self.UID = UID
         UID += 1
 
-    def __eq__(self, o: object) -> bool:
+    def __eq__(self, o: object) -> bool:  # check equivalence only with other User types
         return (
             o
             and self.k_min == o.k_min
@@ -56,11 +78,27 @@ class User:
             and self.race == o.race
         )
 
+    def __hash__(self) -> int:
+        return id(self)
+
     def basicStr(self) -> str:
         fnl = ""
         for occ in self.occupation:
             for _ in range(occ.count):
                 fnl += f"{occ.value}, {self.age.getValue()}, {self.education.getValue()}, {self.marital_status.getValue()}, {self.race.getValue()}\n"
+        return fnl.removesuffix("\n")
+
+    def _privateStr(self) -> str:
+        return f"\t{self._occupation}, {self.age.value}, {self.education.value}, {self.marital_status.value}, {self.race.value}\n"
+
+    def privateStr(self) -> str:
+        fnl = f"KMin: {self.k_min} {self.count}x\n"
+
+        userSet = list(self.userSet)
+        userSet.sort(key=lambda x: x._occupation)
+
+        for usr in userSet:
+            fnl += usr._privateStr()
         return fnl.removesuffix("\n")
 
     def __str__(self) -> str:
@@ -76,6 +114,7 @@ class User:
         if self == user:
             self.count += user.count
             self.occupation = self.occupation.union(user.occupation)
+            self.userSet = self.userSet.union(user.userSet)
             return True
         return False
 
@@ -90,3 +129,11 @@ class User:
 
     def kReached(self) -> bool:
         return self.count >= self.k_min
+
+    def lReached(self) -> bool:
+        if not self.l_min:
+            return True
+        return len(self.occupation) >= self.l_min
+
+    def satisfied(self) -> bool:
+        return self.kReached() and self.lReached()
