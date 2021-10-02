edc8c8236a61efa5293f114d0b3087ef823e56da
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-02 17:45:24 -0500

    main

---
+++ HW2/LaplaceMechanism/main.py
@@ -0,0 +1,20 @@
+# py 3.9.7
+
+"""
+    CS528 - Data Privacy and Security
+    Illinois Institute of Technology
+    Homework 2
+    9-17-21
+    
+    main.py
+    
+    Implementation of differential privacy using the laplace mechanism
+"""
+
+# main function
+def main():
+    print("DP - Laplace Mechanism")
+
+
+if __name__ == "__main__":
+    main()
