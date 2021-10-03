70cf5ae4f7667176cb1b7f6dd93e9fac181d175f
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-03 09:46:53 -0500

    unused func

--- HW2/LaplaceMechanism/main.py
+++ HW2/LaplaceMechanism/main.py
@@ -15,28 +15,6 @@ import pandas as pd
 import numpy as np
 
 
-def getAges(datafile: str) -> list[int]:
-    """
-    Get ages from datafile
-
-    Args:
-        datafile (str): path to datafile
-
-    Returns:
-        list[int]: list of ages
-    """
-    fnl: list[int] = list()
-
-    with open(datafile) as f:
-        for line in f.readlines():
-            try:
-                fnl.append(int(line.split(",")[0]))
-            except ValueError:
-                pass
-
-    return fnl
-
-
 def dpLapDataAvg(eps: float, users: pd.DataFrame):
     """
     Run laplace DP on avg age query
@@ -48,6 +26,7 @@ def dpLapDataAvg(eps: float, users: pd.DataFrame):
 
     users = users[users["age"] > 25]  # Only include those above 25
 
+    # The sensitivity of a function f is the amount f's output changes when its input changes by 1
     sensitivity = 1 / eps
     avg = users["age"].mean()
     lap = np.random.laplace(0, sensitivity, 1)[0]
@@ -65,10 +44,10 @@ def dpLapDataAvg(eps: float, users: pd.DataFrame):
 def main():
     print("\nDP - Laplace Mechanism")
 
-    # users = pd.read_csv("dataset/adult.data")
+    users = pd.read_csv("dataset/adult.data")
 
-    # dpLapDataAvg(1, users)
-    # dpLapDataAvg(0.5, users)
+    dpLapDataAvg(1, users)
+    dpLapDataAvg(0.5, users)
 
 
 if __name__ == "__main__":
