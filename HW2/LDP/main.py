9ef68ed924b77105fcaa5e5a2fdda2ba3810f0a7
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-06 23:14:03 -0500

    better UE implement

--- HW2/LDP/main.py
+++ HW2/LDP/main.py
@@ -14,11 +14,20 @@
 from functools import reduce
 from types import FunctionType
 from typing import Iterable, Iterator
+from multiprocessing import Pool
+from pandas import DataFrame, Series
+from math import exp
+
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-from pandas import DataFrame, Series
-from math import exp
+# Config
+DATA_PATH = "dataset/adult.data"
+VAL_ROUND = 6
+USER_PERCENT_RANGE = 100
+EPSILON_RANGE = 10
 
 
 def normalize(series: Series) -> Series:
@@ -37,7 +46,15 @@ def normalize(series: Series) -> Series:
     return series
 
 
-def LDP_RR(eps: float, users: DataFrame) -> None:
+def LDP_RR(eps: float, users: DataFrame) -> tuple:
+
+    # Used for multiprocessed output
+    global rtnMsg
+    rtnMsg = ""
+
+    def print(msg: str) -> None:
+        global rtnMsg
+        rtnMsg += msg + "\n"
 
     print(f"\n> Random Response | epsilon = {eps}")
 
@@ -51,7 +68,7 @@ def LDP_RR(eps: float, users: DataFrame) -> None:
     prob = normalize(Series((p, q), index=("True Answer", "False Answer")))
 
     print("\nProbabilites:")
-    print(prob.to_string(), "\n")
+    print(prob.to_string() + "\n")
 
     # "Client side"
     def RR(age: int) -> int:
@@ -81,9 +98,11 @@ def LDP_RR(eps: float, users: DataFrame) -> None:
     returned = agesR.mean()
     unbiased = unbiasedEstimate(agesResponse)
 
-    print(f"Actual:  {round(actual, 4)}")
-    print(f"Return:  {round(returned, 4)}  {round(100*(returned-actual)/actual,4)}%")
-    print(f"Unbiased:{round(unbiased, 4)}  {round(100*(unbiased-actual)/actual,4)}%")
+    print(f"Actual:  {round(actual, VAL_ROUND)}")
+    print(f"Return:  {round(returned, VAL_ROUND)}")
+    print(f"Unbiased:{round(unbiased, VAL_ROUND)}")
+
+    return (rtnMsg, eps, unbiased)
 
 
 class UnaryEncoding(list):
@@ -95,14 +114,18 @@ class UnaryEncoding(list):
         UE.extend(list)
         return UE
 
-    def __init__(self, decimal: int = None) -> None:
+    def __init__(self, decimal: int = None, pad: int = None) -> None:
         super().__init__()
-        if not decimal:
+        if decimal == None:
             return
         self.val = decimal
         binaryStr = bin(decimal)[2:]
         for bit in binaryStr:
             self.append(int(bit))
+        if pad:
+            a = self.__pad(self, pad)
+            self.clear()
+            self.extend(a)
 
     def copy(self) -> list[int]:
         return self.from_list(super().copy())
@@ -151,7 +174,16 @@ class UnaryEncoding(list):
         return super().__str__() + " = " + str(self.val)
 
 
-def LDP_UE(eps: float, users: DataFrame) -> None:
+def LDP_UE(eps: float, users: DataFrame) -> tuple:
+
+    # Used for multiprocessed output
+    global rtnMsg
+    rtnMsg = ""
+
+    def print(msg: str) -> None:
+        global rtnMsg
+        rtnMsg += msg + "\n"
+
     print(f"\n> Unary Encoding | epsilon = {eps}")
 
     e = exp(eps / 2)
@@ -159,9 +191,10 @@ def LDP_UE(eps: float, users: DataFrame) -> None:
     q = 1 / (e + 1)  # Ch.4 Slide 43
     prob = normalize(Series((p, q), index=("Keep", "Flip")))
     p, q = prob
+    n = len(users)
 
     print("\nProbabilites:")
-    print(prob.to_string(), "\n")
+    print(prob.to_string() + "\n")
 
     # "Client side"
     def UE(bit: int) -> int:
@@ -170,33 +203,42 @@ def LDP_UE(eps: float, users: DataFrame) -> None:
         else:
             return 1 if bit == 0 else 0
 
-    ages = users["age"].apply(lambda x: UnaryEncoding(x))
-    agesEncodedActual: UnaryEncoding = reduce(lambda x, y: x + y, ages)
+    uAges = list(set(users["age"]))
+    uAges.sort()
+
+    binAges = dict()
+    for i in range(len(uAges)):
+        binAges[uAges[i]] = UnaryEncoding(1 << i, len(uAges))
+
+    ages = users["age"].apply(lambda x: binAges[x])
+    agesEncodedActual: DataFrame = reduce(lambda x, y: x + y, ages)
+    agesEncodedActual = Series(agesEncodedActual[::-1])
+
     ages = ages.apply(lambda x: x.apply(UE))
-    agesEncodedReturn: UnaryEncoding = reduce(lambda x, y: x + y, ages)  # "Return" value
+    agesEncodedReturn: DataFrame = reduce(lambda x, y: x + y, ages)  # "Return" value
+    agesEncodedReturn = Series(agesEncodedReturn[::-1])
 
     # "Server side"
 
-    # global _i
-    # _i = 0
-    n = len(agesEncodedReturn)
-
     def unbias(i_v: int) -> int:
-        # global _i
-        # v = agesEncodedActual[_i]
-        # i = agesEncodedReturn[_i]
-        # _i += 1
-        # n = (-2 * v * p + v + i) / q
-        # print(n / len(users))
-        # print(round(n), round(v - n))
-        # n = i_v * 2
-        return round((i_v - (n * q)) / (p - q))  # Ch.4 Slide 41
+        return (i_v - (n * q)) / (p - q)  # Ch.4 Slide 41
 
-    agesEncodedUnBiased: UnaryEncoding = agesEncodedReturn.apply(unbias)
+    agesEncodedUnBiased: Series = agesEncodedReturn.apply(unbias)
 
-    print(f"Actual:  {agesEncodedActual}")
-    print(f"Return:  {agesEncodedReturn}  {agesEncodedActual.compare(agesEncodedReturn)}%")
-    print(f"Unbiased:{agesEncodedUnBiased}  {agesEncodedActual.compare(agesEncodedUnBiased)}%")
+    for i in range(len(agesEncodedActual)):
+        agesEncodedActual[i] *= uAges[i]
+        agesEncodedReturn[i] *= uAges[i]
+        agesEncodedUnBiased[i] *= uAges[i]
+
+    actual = sum(agesEncodedActual) / n
+    returned = sum(agesEncodedReturn) / n
+    unbiased = sum(agesEncodedUnBiased) / n
+
+    print(f"Actual:  {round(actual, VAL_ROUND)}")
+    print(f"Return:  {round(returned, VAL_ROUND)}")
+    print(f"Unbiased:{round(unbiased, VAL_ROUND)}")
+
+    return (rtnMsg, eps, unbiased)
 
 
 # Main Function
@@ -204,13 +246,76 @@ def main():
     print("\nLocal differential privacy")
 
     # Make prob output look nicer
-    pd.options.display.float_format = "{:10,.6f}".format
+    pd.options.display.float_format = ("{" + f":10,.{VAL_ROUND}f" + "}").format
 
-    users = pd.read_csv("dataset/adult.data", skipinitialspace=True)
+    global users
+    users = pd.read_csv(DATA_PATH, skipinitialspace=True)
 
-    for i in range(10):
-        LDP_RR(i + 1, users)
-        LDP_UE(i + 1, users)
+    actual = users["age"].mean()
+
+    print("----[ Task 4(b) ]----")
+
+    UEEst = [0] * EPSILON_RANGE
+    RREst = [0] * EPSILON_RANGE
+
+    with Pool(EPSILON_RANGE) as p:
+        for msg in p.starmap(LDP_UE, [(i, users) for i in range(1, EPSILON_RANGE + 1)]):
+            print(msg[0])
+            UEEst[msg[1] - 1] = msg[2]
+        for msg in p.starmap(LDP_RR, [(i, users) for i in range(1, EPSILON_RANGE + 1)]):
+            print(msg[0])
+            RREst[msg[1] - 1] = msg[2]
+
+    UEEst_E = [round(abs(e - actual), VAL_ROUND) for e in UEEst]
+    RREst_E = [round(abs(e - actual), VAL_ROUND) for e in RREst]
+    x = list(range(1, EPSILON_RANGE + 1))
+
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots()
+    UE_patch = mpatches.Patch(color="red", label="Unary Encoding")
+    RR_patch = mpatches.Patch(color="blue", label="Random Response")
+    ax.set_xlabel("Epsilon")
+    ax.set_ylabel("L_1 - distance (Age)")
+    ax.set_title("LDP Error Frequency (Avg Age)")
+    plt.plot(x, UEEst_E, "ro-")
+    plt.plot(x, RREst_E, "bo-")
+    fig.savefig("LDP/error_plot_eps.png")
+
+    print("----[ Task 4(c) ]----")
+
+    nDiffC = int(USER_PERCENT_RANGE / 10)
+    eps = 2
+    n = len(users)
+    n10 = int(n * 0.1)
+    nMax = int(n * USER_PERCENT_RANGE / 100) + 1
+
+    UEEst = [0] * nDiffC
+    RREst = [0] * nDiffC
+
+    with Pool(nDiffC) as p:
+        for msg in p.starmap(LDP_UE, [(eps, users[:i]) for i in range(n10, nMax, n10)]):
+            print(msg[0])
+            UEEst[msg[1] - 1] = msg[2]
+        for msg in p.starmap(LDP_RR, [(eps, users[:i]) for i in range(n10, nMax, n10)]):
+            print(msg[0])
+            RREst[msg[1] - 1] = msg[2]
+
+    UEEst_E = [round(abs(e - actual), VAL_ROUND) for e in UEEst]
+    RREst_E = [round(abs(e - actual), VAL_ROUND) for e in RREst]
+    x = list(range(n10, nMax, n10))
+
+    plt.figure()
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots()
+    UE_patch = mpatches.Patch(color="red", label="Unary Encoding")
+    RR_patch = mpatches.Patch(color="blue", label="Random Response")
+    plt.legend(handles=[UE_patch, RR_patch])
+    ax.set_xlabel("# of Users")
+    ax.set_ylabel("L_1 - distance (Age)")
+    ax.set_title("LDP Error Frequency (Avg Age)")
+    plt.plot(x, UEEst_E, "ro-")
+    plt.plot(x, RREst_E, "bo-")
+    fig.savefig("LDP/error_plot_num.png")
 
 
 if __name__ == "__main__":
